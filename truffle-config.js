99ff7f65744d2e8d0600bbb658351b3650478482
Seth Landry <waymakerijn@outlook.com>
2025-01-10 11:43:32 -0600

    Update Truffle configuration for compiler version
    * Set the compiler version to 0.8.0 in the Truffle configuration file

--- truffle-config.js
+++ truffle-config.js
@@ -6,10 +6,9 @@ module.exports = {
       network_id: "*", // Match any network id
     },
   },
-
   compilers: {
     solc: {
-      version: "0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.0", // Fetch exact version from solc-bin
     },
   },
 };
