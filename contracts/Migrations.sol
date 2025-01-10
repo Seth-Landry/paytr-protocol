f6e90a77612593d5851802cd3470a4e96e6efa01
Seth Landry <waymakerijn@outlook.com>
2025-01-09 20:21:43 -0600

    Update `Migrations.sol` to use Solidity 0.8.0
    * Change pragma statement to specify Solidity version 0.8.0
    * Remove `public` visibility specifier from constructor

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,10 +1,10 @@
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() public {
+  constructor() {
     owner = msg.sender;
   }
 
