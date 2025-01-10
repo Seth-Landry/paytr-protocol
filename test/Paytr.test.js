f3d33134d7b00aa5d71b023d94ddc50dec4d59c3
Seth Landry <waymakerijn@outlook.com>
2025-01-09 21:06:43 -0600

    Add tests for deposit and withdrawal functions in Paytr contract
    * Update test descriptions for deposit and withdrawal functions
    * Add test for emitting Deposit event on deposit
    * Add test for emitting Withdrawal event on withdrawal

--- test/Paytr.test.js
+++ test/Paytr.test.js
@@ -7,7 +7,7 @@ contract("Paytr", (accounts) => {
     paytrInstance = await Paytr.new();
   });
 
-  it("should allow a user to deposit funds", async () => {
+  it("should allow users to deposit funds", async () => {
     const depositAmount = web3.utils.toWei("1", "ether");
     await paytrInstance.deposit({ from: accounts[0], value: depositAmount });
 
@@ -15,38 +15,38 @@ contract("Paytr", (accounts) => {
     assert.equal(balance.toString(), depositAmount, "Deposit amount is incorrect");
   });
 
-  it("should allow a user to withdraw funds", async () => {
+  it("should emit a Deposit event when funds are deposited", async () => {
     const depositAmount = web3.utils.toWei("1", "ether");
-    const withdrawalAmount = web3.utils.toWei("0.5", "ether");
+    const receipt = await paytrInstance.deposit({ from: accounts[0], value: depositAmount });
 
+    assert.equal(receipt.logs.length, 1, "Deposit event should be emitted");
+    assert.equal(receipt.logs[0].event, "Deposit", "Event name should be Deposit");
+    assert.equal(receipt.logs[0].args.user, accounts[0], "Event user should be the depositor");
+    assert.equal(receipt.logs[0].args.amount.toString(), depositAmount, "Event amount should be the deposited amount");
+  });
+
+  it("should allow users to withdraw funds", async () => {
+    const depositAmount = web3.utils.toWei("1", "ether");
     await paytrInstance.deposit({ from: accounts[0], value: depositAmount });
-    await paytrInstance.withdraw(withdrawalAmount, { from: accounts[0] });
+
+    const withdrawAmount = web3.utils.toWei("0.5", "ether");
+    await paytrInstance.withdraw(withdrawAmount, { from: accounts[0] });
 
     const balance = await paytrInstance.getBalance({ from: accounts[0] });
     const expectedBalance = web3.utils.toWei("0.5", "ether");
     assert.equal(balance.toString(), expectedBalance, "Withdrawal amount is incorrect");
   });
 
-  it("should emit Deposit event on deposit", async () => {
+  it("should emit a Withdrawal event when funds are withdrawn", async () => {
     const depositAmount = web3.utils.toWei("1", "ether");
-    const receipt = await paytrInstance.deposit({ from: accounts[0], value: depositAmount });
-
-    assert.equal(receipt.logs.length, 1, "Deposit event not emitted");
-    assert.equal(receipt.logs[0].event, "Deposit", "Deposit event name is incorrect");
-    assert.equal(receipt.logs[0].args.user, accounts[0], "Deposit event user is incorrect");
-    assert.equal(receipt.logs[0].args.amount.toString(), depositAmount, "Deposit event amount is incorrect");
-  });
-
-  it("should emit Withdrawal event on withdrawal", async () => {
-    const depositAmount = web3.utils.toWei("1", "ether");
-    const withdrawalAmount = web3.utils.toWei("0.5", "ether");
-
     await paytrInstance.deposit({ from: accounts[0], value: depositAmount });
-    const receipt = await paytrInstance.withdraw(withdrawalAmount, { from: accounts[0] });
 
-    assert.equal(receipt.logs.length, 1, "Withdrawal event not emitted");
-    assert.equal(receipt.logs[0].event, "Withdrawal", "Withdrawal event name is incorrect");
-    assert.equal(receipt.logs[0].args.user, accounts[0], "Withdrawal event user is incorrect");
-    assert.equal(receipt.logs[0].args.amount.toString(), withdrawalAmount, "Withdrawal event amount is incorrect");
+    const withdrawAmount = web3.utils.toWei("0.5", "ether");
+    const receipt = await paytrInstance.withdraw(withdrawAmount, { from: accounts[0] });
+
+    assert.equal(receipt.logs.length, 1, "Withdrawal event should be emitted");
+    assert.equal(receipt.logs[0].event, "Withdrawal", "Event name should be Withdrawal");
+    assert.equal(receipt.logs[0].args.user, accounts[0], "Event user should be the withdrawer");
+    assert.equal(receipt.logs[0].args.amount.toString(), withdrawAmount, "Event amount should be the withdrawn amount");
   });
 });
