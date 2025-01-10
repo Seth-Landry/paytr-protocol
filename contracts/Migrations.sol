83d23abda8c9ac675b6c1d53634df47f6c4f12df
Seth Landry <waymakerijn@outlook.com>
2025-01-09 21:11:37 -0600

    Update `Migrations` contract to Solidity 0.8.0 and refactor
    * Change Solidity version to 0.8.0
    * Refactor `restricted` modifier to use `if` statement
    * Move `restricted` modifier above the constructor
    * Adjust indentation and formatting for consistency

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,18 @@
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+    address public owner;
+    uint public last_completed_migration;
 
-  constructor() {
-    owner = msg.sender;
-  }
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
 
-  modifier restricted() {
-    require(msg.sender == owner, "This function is restricted to the contract's owner");
-    _;
-  }
+    constructor() {
+        owner = msg.sender;
+    }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
+    }
 }
