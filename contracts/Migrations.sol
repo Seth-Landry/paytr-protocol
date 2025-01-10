48cf4f05c068916bb69089a7d0b870e531a70c3a
Seth Landry <waymakerijn@outlook.com>
2025-01-09 21:15:51 -0600

    Add constructor to `Migrations` contract
    * Define the constructor to set the owner as the message sender
    * Move the constructor to the correct position in the contract

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -4,13 +4,13 @@ contract Migrations {
     address public owner;
     uint public last_completed_migration;
 
-    constructor() {
-        owner = msg.sender;
-    }
-
     modifier restricted() {
         require(msg.sender == owner, "This function is restricted to the contract's owner");
         _;
+    }
+
+    constructor() {
+        owner = msg.sender;
     }
 
     function setCompleted(uint completed) public restricted {
